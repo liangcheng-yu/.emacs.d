911f499380f29fb7c910667702a9c953b82076f2
liangcheng-yu <liangcheng.yu46@gmail.com>
2018-10-26 22:22:44 -0700

    Add config with babel

--- init.el
+++ init.el
@@ -5,28 +5,6 @@
 (when (version< emacs-version "26.1")
   (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
-;; Measure startup time
-(add-to-list 'load-path "~/.emacs.d/modules/")
-(require 'benchmarking)
-
-;; prefer material theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/modules/themes/")
-(load-theme 'material t)
-
-(require 'basic)
-(require 'org-mode)
-(require 'key-bindings)
-
-;; babel
-;; to simplify init.el, it will look over the doc and evaluate all code blocks that are emacs lisp sequentially
-;; (org-babel-load-file "~/.emacs.d/config.org")
-
-;; UI perferences
-(require 'ui)
-
-(require 'dired)
-(require 'buffer)
-
 ;; add packet archive melpa
 (when (>= emacs-major-version 24)
      (require 'package)
@@ -40,17 +18,7 @@
 ;; manually install packages via:
 ;; M-x package-install RET markdown-mode
 
-;; set agenda related
-(require 'agenda)
+(org-babel-load-file "~/.emacs.d/config.org")
 
-;; org bullets settings
-;; customize org-bullet symbol set https://github.com/sabof/org-bullets
-(add-to-list 'load-path "~/.emacs.d/modules/")
-;; use org-bullets-mode for utf8 symbols as org bullets
-(require 'org-bullets)
 
-(add-to-list 'load-path "~/.emacs.d/modules/lang/")
-(require 'cpp)
 
-(add-to-list 'load-path "~/.emacs.d/modules/app/")
-(require 'web-browsing)
